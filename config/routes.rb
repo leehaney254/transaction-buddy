faf622d254a05869ef4e3da564a82984801cc30d
Leehaney gee <georgeleehaney20@gmail.com>
2023-04-21 19:58:24 +0300

    Feat: Redirect user login to categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
   devise_for :users
   resources :transactions
   resources :categories
   resources :users
-  devise_scope :user do
-    get '/users/sign_out', to: 'devise/sessions#destroy'
-  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
