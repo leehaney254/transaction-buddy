dc0a1f6cc38dbba45237c703f4de8adcd943e395
Leehaney gee <georgeleehaney20@gmail.com>
2023-04-21 18:07:39 +0300

    Feat: Create Login and sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :transactions
   resources :categories
   resources :users
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
