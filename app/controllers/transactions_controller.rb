d9027eff562048d3fd0fe47dd3b93663c2331ba9
Leehaney gee <georgeleehaney20@gmail.com>
2023-04-23 12:56:20 +0300

    Feat: Add total for transactions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -3,7 +3,7 @@ class TransactionsController < ApplicationController
 
   # GET /transactions or /transactions.json
   def index
-    @transactions = Transaction.all
+    @transactions = Transaction.order(created_at: :desc)
   end
 
   # GET /transactions/1 or /transactions/1.json
