fe1acd157e99cc383726cb5120e194d4376895e4
Leehaney gee <georgeleehaney20@gmail.com>
2023-04-23 19:29:30 +0300

    Fix linters

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -27,7 +27,9 @@ class TransactionsController < ApplicationController
 
     respond_to do |format|
       if @transaction.save
-        format.html { redirect_to transactions_path(transaction_category), notice: 'Transaction was successfully created.' }
+        format.html do
+          redirect_to transactions_path(transaction_category), notice: 'Transaction was successfully created.'
+        end
         format.json { render :show, status: :created, location: @transaction }
       else
         format.html { render :new, status: :unprocessable_entity }
