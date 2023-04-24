9886d7eb21b35bd8145ea5338dfdab213ffae0b3
Leehaney gee <georgeleehaney20@gmail.com>
2023-04-24 12:40:07 +0300

    Fix linters

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "Categories", type: :request do
-  describe "GET /categories" do
-    it "redirects to sign-in if user is not logged in" do
+RSpec.describe 'Categories', type: :request do
+  describe 'GET /categories' do
+    it 'redirects to sign-in if user is not logged in' do
       get categories_path
       expect(response).to redirect_to(new_user_session_path)
     end
